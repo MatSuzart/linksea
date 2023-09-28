7900fc84cd1810685fdbaac063a77aa12d99d144
Matheus Suzart <mathsuzart@outlook.com>
2023-09-28 00:02:28 -0300

    page-controller-init

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,10 +8,20 @@ use App\Page;
 class PageController extends Controller
 {
     public function index($slug){
-        $page = Page::where('slug',$slug)->frist();
+        //$page = Page::where('slug',$slug)->frist();
+
+        $links = [];
 
         if($page){
-            echo 'PAGE';
+            return view('page',[
+                'font_color'=>$page->op_font_color,
+                'profile_image'=>url('media/uploads').'/'.$page->op_profile_image,
+                'title'=>$page->op_title,
+                'description'=>$page->op_description,
+                'fb_pixel'=>$page->op_fb_pixel,
+                'bg'=>$bg,
+                'links'=>$links
+            ]);
         }else{
             return view('notfound');
         }
