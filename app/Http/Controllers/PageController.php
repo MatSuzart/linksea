73c62dd5a552a956e315f7159515743e496645ec
Matheus Suzart <mathsuzart@outlook.com>
2024-01-14 23:52:43 -0300

    links

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Page;
-
+use App\link;
 class PageController extends Controller
 {
     public function index($slug){
@@ -16,9 +16,24 @@ class PageController extends Controller
 
             $bg = '';
 
-            
+            switch($page->op_bg_type){
+                case 'image':
+                    $bg = "url('".url('/media/uploads').'/'.$page->op_bg_value."')";
+                    break;
+                case 'color':
+                    $colors = explode(',',$page->op_bg_value);
+                    $bg = 'lienar-gradient(90deg,';
+                    $bg .= $colors[0].',';
+                    $bg .= !empty($colors[1]) ? $colors[1] : $colors[0];
 
-            $links = [];
+                    $bg .= ')';
+                    break;
+            }
+
+            $links = Link::where('id_page', $page->id)
+            ->where('status', 1)
+            ->orderBy('order')
+            ->get();
 
             return view('page',[
                 'font_color'=>$page->op_font_color,
